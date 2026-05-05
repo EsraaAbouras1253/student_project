73f9e8a59fb1a3af695ddaeaf6cf59cdb6711efb
esraa <abourasesraa@gmail.com>
2026-05-06 01:00:00 +0200

    Update project files

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <?php
 require_once "Helpers/headers.php";
-send_json_api_headers('POST');
+send_json_api_headers('GET');
 require_once "Config/db.php"; 
 require_once "Helpers/response.php";
 
 // رفض أي طلب نوع اتصاله ليس GET
-if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+if ($_SERVER["REQUEST_METHOD"] !== "GET") {
     response(405, "Only GET Method is allowed");
+    exit ;
 }
-
-try {
-    $email = $_POST['email']; // استلام الإيميل من الطلب
-$checkQuery = "SELECT id FROM students WHERE email = ?";
-$checkSql = $pdo->prepare($checkQuery);
-$checkSql->execute([$email]);
-if ($checkSql->fetch()) {
-    response(400, "هذا الإيميل موجود مسبقاً، يرجى استخدام إيميل آخر");
-}
-    // تعديل الاستعلام ليناسب جدول الطلاب
+try{
     $query = "SELECT * FROM `students`"; 
     $sql = $pdo->prepare($query);
     $sql->execute();
